5f4092e0a81eb40b0763a9ecb072902d5429c25b
Shehryar Ahmed <shehryar.ahmed@utexas.edu>
2019-02-22 15:23:25 -0600

    changed some basic UI

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HelloComponent} from './hello/hello.component';
+import {AppComponent} from './app.component';
 
 const routes: Routes = [
   { path: 'hello', component: HelloComponent },
-  {path:"**",component:HelloComponent}
+ // {path:"**",component:AppComponent}
   //{ path: '', redirectTo: '/hello', pathMatch: 'full'}
 ];
 
