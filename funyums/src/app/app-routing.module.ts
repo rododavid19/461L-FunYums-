6337e2433ab8bd4f05004ba61253f163beaefa53
HadarRoze <hadarroze@utexas.edu>
2019-02-25 18:57:41 -0600

    Recipe search and routing setup

--- funyums/src/app/app-routing.module.ts
+++ funyums/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { RecipeSearchComponent} from './recipe-search/recipe-search.component';
+
+// we need a dashboard component to add here
+const routes: Routes = [
+  {path: 'search', component: RecipeSearchComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- funyums/src/app/app.module.ts
+++ funyums/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeSearchComponent } from './recipe-search/recipe-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeSearchComponent
   ],
   imports: [
     BrowserModule,
