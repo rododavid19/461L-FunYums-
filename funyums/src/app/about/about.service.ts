ce8071cb6fd68719cdc2cd2f715e8403857842fd
Ashkan Vafaee <ashkanvafaee@utexas.edu>
2019-03-25 00:26:20 -0500

    Fixed max 30 commits problem

--- funyums/src/app/about/about.service.ts
+++ funyums/src/app/about/about.service.ts
@@ -7,7 +7,7 @@ import {issues} from './commits-interface';
 @Injectable()
 export class AboutService{
 
-    commit_url = "https://api.github.com/repos/rododavid19/461L-FunYums-/commits?access_token=767f2b5bf0f53943d9b033dbeebaa75f4746928a";
+    commit_url = "https://api.github.com/repos/rododavid19/461L-FunYums-/commits?per_page=100&access_token=767f2b5bf0f53943d9b033dbeebaa75f4746928a";
 
     issue_url = "https://api.github.com/repos/rododavid19/461L-FunYums-/issues?state=all&access_token=767f2b5bf0f53943d9b033dbeebaa75f4746928a";
 
