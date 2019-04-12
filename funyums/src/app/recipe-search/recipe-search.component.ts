ca7ad320105adccadce2578f4db148d4984b24a1
HadarRoze <hadarroze@utexas.edu>
2019-04-12 15:01:17 -0500

    basic aesthetics
    basic now appear, but require further styling and are not yet functional

--- funyums/src/app/recipe-search/recipe-search.component.ts
+++ funyums/src/app/recipe-search/recipe-search.component.ts
@@ -11,6 +11,14 @@ export class RecipeSearchComponent implements OnInit {
 
   recipes: RecipeSearchBar[];
   recipesShow = false;
+  showAdvance = false;
+  onlyMyIng = false;
+  ings2excludeExist = false;
+  ings2exclude: string[];
+  allergiesExist = false;
+  allergies: string[];
+  dietsExist = false;
+  diets: string[];
 
   constructor(private recipeGetter: RecipesGetterService) { }
 
@@ -34,9 +42,65 @@ export class RecipeSearchComponent implements OnInit {
     }
   }
 
-  /*getRecipes(txt: string): void {
-    console.log(txt);
-    this.recipeGetter.getRecipes().subscribe(recipes => this.recipes = recipes);
-  }*/
+
+  // this also needs to make sure that the input is valid
+  addIng(ingName: string): void {
+    if (ingName === '') {
+      return;
+    }
+    if (this.ings2excludeExist === false) {
+      this.ings2excludeExist = true;
+      this.ings2exclude = [];
+    }
+    this.ings2exclude.push(ingName);
+  }
+
+  removeIng(ingName: string): void {
+    const indx = this.ings2exclude.indexOf(ingName);
+    this.ings2exclude.splice(indx, 1);
+    if (this.ings2exclude.length === 0) {
+      this.ings2excludeExist = false;
+    }
+  }
+
+  // this also needs to make sure that the input is valid
+  addAllergy(allergyName: string): void {
+    if (allergyName === '') {
+      return;
+    }
+    if (this.allergiesExist === false) {
+      this.allergiesExist = true;
+      this.allergies = [];
+    }
+    this.allergies.push(allergyName);
+  }
+
+  removeAllergy(allergyName: string): void {
+    const indx = this.allergies.indexOf(allergyName);
+    this.allergies.splice(indx, 1);
+    if (this.allergies.length === 0) {
+      this.allergiesExist = false;
+    }
+  }
+
+  // this also needs to make sure that the input is valid
+  addDiet(dietName: string): void {
+    if (dietName === '') {
+      return;
+    }
+    if (this.dietsExist === false) {
+      this.dietsExist = true;
+      this.diets = [];
+    }
+    this.diets.push(dietName);
+  }
+
+  removeDiet(dietName: string): void {
+    const indx = this.diets.indexOf(dietName);
+    this.diets.splice(indx, 1);
+    if (this.diets.length === 0) {
+      this.dietsExist = false;
+    }
+  }
 
 }
