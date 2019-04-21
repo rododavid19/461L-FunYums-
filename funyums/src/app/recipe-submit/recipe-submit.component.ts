e5777d9700a3916d0a1ec27ec04d661ddd98b69f
Christian <christianhan@utexas.edu>
2019-04-21 17:12:19 -0500

    added ingredient add/remove functionality

--- funyums/src/app/recipe-submit/recipe-submit.component.ts
+++ funyums/src/app/recipe-submit/recipe-submit.component.ts
@@ -1,6 +1,7 @@
   import { Component, OnInit } from '@angular/core';
   import { Recipe } from '../recipe';
   import {RecipesGetterService} from '../recipes-getter.service';
+
   import {FormArray, FormBuilder, FormControl, FormGroup} from "@angular/forms";
   import { of } from 'rxjs';
 
@@ -9,65 +10,33 @@
   templateUrl: './recipe-submit.component.html',
   styleUrls: ['./recipe-submit.component.css']
 })
+
+
+
 export class RecipeSubmitComponent implements OnInit {
 
 
+  constructor(private recipeGetter: RecipesGetterService) { }
 
-
-  item:string;
-  constructor() {
-    this.item = 'hello';
-  }
 
   submitted = false;
 
-  onSubmit() { this.submitted = true; }
+  //onSubmit() { this.submitted = true; }
 
 
   ngOnInit() {
   }
-}
 
-const orders = [
-  { id: 1, name: 'Cow&#39s Milk' },
-  { id: 2, name: 'Eggs' },
-  { id: 3, name: 'Tree Nuts' },
-  { id: 4, name: 'Peanuts' },
-  { id: 5, name: 'Shellfish' },
-  { id: 6, name: 'Wheat' },
-  { id: 7, name: 'Soy' },
-  { id: 8, name: 'Fish' },
-];
-
-
-
-@Component({
-  selector: 'app-recipe-submit',
-  templateUrl: './recipe-submit.component.html',
-  styleUrls: ['./recipe-submit.component.css']
-})
-export class AppComponent {
   form: FormGroup;
   orders = [];
 
   ings2excludeExist = false;
   ings2exclude: string[];
 
-  constructor(private formBuilder: FormBuilder) {
-    this.form = this.formBuilder.group({
-      orders: new FormArray([])
-    });
 
-    // async orders (could be a http service call)
-    of(this.getOrders()).subscribe(orders => {
-      this.orders = orders;
-      this.addCheckboxes();
-    });
+  initSearch: string;
 
-    // synchronous orders
-    this.orders = this.getOrders();
-    this.addCheckboxes();
-  }
+
 
   private addCheckboxes() {
     this.orders.map((o, i) => {
@@ -98,19 +67,113 @@ export class AppComponent {
       this.ings2excludeExist = true;
       this.ings2exclude = [];
     }
+
+
     this.ings2exclude.push(ingName);
   }
 
   removeIng(ingName: string): void {
     const indx = this.ings2exclude.indexOf(ingName);
     this.ings2exclude.splice(indx, 1);
-    if (this.ings2exclude.length === 0) {
-      this.ings2excludeExist = false;
+    // if (this.ings2exclude.length === 0) {
+    //   this.ings2excludeExist = false;
+    // }
+  }
+
+  saveLocalState(): void{
+    if(this.initSearch !== ''){
+      localStorage.removeItem('SearchBar');
+      localStorage.setItem('SearchBar', JSON.stringify(this.initSearch));
     }
   }
 
-  // submit() {}
+
+
 }
+
+
+  const orders = [
+    { id: 1, name: 'Cow&#39s Milk' },
+    { id: 2, name: 'Eggs' },
+    { id: 3, name: 'Tree Nuts' },
+    { id: 4, name: 'Peanuts' },
+    { id: 5, name: 'Shellfish' },
+    { id: 6, name: 'Wheat' },
+    { id: 7, name: 'Soy' },
+    { id: 8, name: 'Fish' },
+  ];
+
+
+@Component({
+  selector: 'app-recipe-submit',
+  templateUrl: './recipe-submit.component.html',
+  styleUrls: ['./recipe-submit.component.css']
+})
+export class AppComponent {
+//   form: FormGroup;
+//   orders = [];
+//
+//   ings2excludeExist = false;
+//   ings2exclude: string[];
+//
+//   constructor(private formBuilder: FormBuilder) {
+//     this.form = this.formBuilder.group({
+//       orders: new FormArray([])
+//     });
+//
+//     // async orders (could be a http service call)
+//     of(this.getOrders()).subscribe(orders => {
+//       this.orders = orders;
+//       this.addCheckboxes();
+//     });
+//
+//     // synchronous orders
+//     this.orders = this.getOrders();
+//     this.addCheckboxes();
+//   }
+//
+//   private addCheckboxes() {
+//     this.orders.map((o, i) => {
+//       const control = new FormControl(i === 0); // if first item set to true, else false
+//       (this.form.controls.orders as FormArray).push(control);
+//     });
+//   }
+//
+//   getOrders() {
+//     return [
+//       { id: 100, name: 'Cow&#39s Milk' },
+//       { id: 200, name: 'Eggs' },
+//       { id: 300, name: 'Tree Nuts' },
+//       { id: 400, name: 'Peanuts' },
+//       { id: 500, name: 'Shellfish' },
+//       { id: 600, name: 'Wheat' },
+//       { id: 700, name: 'Soy' },
+//       { id: 800, name: 'Fish' },
+//     ];
+//   }
+//
+//
+//   addIng(ingName: string): void {
+//     if (ingName === '') {
+//       return;
+//     }
+//     if (this.ings2excludeExist === false) {
+//       this.ings2excludeExist = true;
+//       this.ings2exclude = [];
+//     }
+//     this.ings2exclude.push(ingName);
+//   }
+//
+//   removeIng(ingName: string): void {
+//     const indx = this.ings2exclude.indexOf(ingName);
+//     this.ings2exclude.splice(indx, 1);
+//     if (this.ings2exclude.length === 0) {
+//       this.ings2excludeExist = false;
+//     }
+//   }
+//
+//   // submit() {}
+ }
 
 
 
