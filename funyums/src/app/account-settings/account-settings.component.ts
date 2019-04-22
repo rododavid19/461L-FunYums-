4321d82aa0bc01d1f12294c13633e0e1deb2fd4c
Shehryar Ahmed <shehryar.ahmed@utexas.edu>
2019-04-22 14:10:58 -0500

    updated account settings to display ingredients and recipes

--- funyums/src/app/account-settings/account-settings.component.ts
+++ funyums/src/app/account-settings/account-settings.component.ts
@@ -13,16 +13,23 @@ export class AccountSettingsComponent implements OnInit {
   uname:any;
   name: any;
   account_rank:any;
-  recipes:any;
+  favorites:any;
   public person : person;
+  ingredients: any;
+  addThisIngredient:any;
 
   constructor(private router: Router) { }
 
   ngOnInit() {
 
+
+
+
     if(AppComponent.getFromLocal("local") != null){
       this.person = AppComponent.getFromLocal("local");
+      console.log("person"+this.person);
     }
+    
 
     if(this.person.email == null){
       alert('Need to login to do this')
@@ -31,7 +38,50 @@ export class AccountSettingsComponent implements OnInit {
     this.uname = this.person.email;
     this.name = this.person.fullname
     this.account_rank = this.person.rank;
-    this.recipes = this.person.favorites;
+    this.ingredients = this.person.ingredients;
+
+    if(this.person.ingredients == null ||this.person.ingredients == ""){
+
+      this.ingredients = "";
+    }else{
+    
+      
+      console.log(this.person.ingredients);
+      var listIngredients = [];
+      var IngredientsString = this.person.ingredients.split(" ");
+      console.log(IngredientsString);
+      this.ingredients = IngredientsString;
+    }
+    if(this.person.favorites == null ||this.person.favorites == ""){
+
+      this.favorites = "";
+    }else{
+    
+      
+      console.log(this.person.favorites);
+      var listFav = [];
+      var FavssString = this.person.favorites.split(" ");
+      console.log(FavssString);
+      this.favorites = FavssString;
+    }
+
+
+
+  }
+
+  addIngredient(){
+    console.log(this.addThisIngredient);
+    console.log(this.ingredients);
+    
+    if(this.ingredients == null || this.ingredients == ""){ //if user does not have ingredients 
+      this.ingredients = this.addThisIngredient;
+      console.log(this.ingredients);
+    }
+    else { // if they have ingredients 
+      this.ingredients = this.ingredients + ", "+this.addThisIngredient;
+      console.log(this.ingredients);
+    }
+    
   }
 
 }
